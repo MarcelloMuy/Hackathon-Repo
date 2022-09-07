7a039ed6fbd63c383dd8e5bee63454e9f80bbcc1
MarcelloMuy <marcellomuylaert@icloud.com>
2022-09-07 20:42:07 +0000

    Added a receiver to create or update the UserProfile model

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-
+"""Imported"""
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+@login_required
 def user_profile(request):
     """This view returns the user profile page."""
-    return render(request, 'userprofile/userprofile.html')
+    myprofile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'userprofile/userprofile.html'
+    context = {
+        'myprofile': myprofile,
+    }
+
+    return render(request, template, context)
