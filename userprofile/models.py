fe8e42eb7976e805bdc7c0857f54668207ff5519
MarcelloMuy <marcellomuylaert@icloud.com>
2022-09-07 19:19:14 +0000

    Create userprofile model

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,11 @@
+"""Imported"""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """A user profile model"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
